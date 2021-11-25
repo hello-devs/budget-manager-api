6bbf32496c0084081f82ed08290b1598c336dc15
hello-devs <cirer972@gmail.com>
2021-11-25 23:32:54 +0100

    usecase: continue to write this functional test by preparing persist a user in database
    ask entity manager to persist user
    refactoring $hasher creation
    manage entity manager transaction to be able to roll back operation after test

--- tests/Api/Security/AuthenticationITest.php
+++ tests/Api/Security/AuthenticationITest.php
@@ -3,34 +3,34 @@
 namespace Tests\Api\Security;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AuthenticationITest extends WebTestCase
 {
-    private ?object $hasher;
+    private UserPasswordHasherInterface $hasher;
+    private EntityManagerInterface $entityManager;
 
     /** @test
      * @throws Exception
      */
     public function user_should_be_able_to_get_jwt_token_when_authenticated_with_correct_login_and_password(): void
     {
-        if (!$this->hasher instanceof UserPasswordHasher) {
-            throw new Exception("hasher must be a instance of UserPasswordHasher class");
-        }
-
         //We have a user in the database
         $user = new User();
         $pwd = $this->hasher->hashPassword($user, 'pwd');
 
-
         $user
             ->setUsername('tester')
             ->setEmail('tester@email.com')
-            ->setPassword($pwd);
+            ->setPassword($pwd)
+        ;
 
-
+        //Todo: map User class with doctrine to fix error
+        $this->entityManager->persist($user);
 
         //When the user request the api login endpoint
 //        $client = static::createClient();
@@ -41,8 +41,28 @@ class AuthenticationITest extends WebTestCase
 
     protected function setUp(): void
     {
+        parent::setUp();
+
         self::bootKernel();
+
         $container = static::getContainer();
-        $this->hasher = $container->get('security.user_password_hasher');
+        /** @var UserPasswordHasherInterface $hasher */
+        $hasher = $container->get('security.user_password_hasher');
+        $this->hasher = $hasher;
+
+        /** @var EntityManagerInterface $em */
+        $em = $container->get('doctrine.orm.entity_manager');
+        $this->entityManager = $em;
+        $this->entityManager->beginTransaction();
+        $this->entityManager->getConnection()->setAutoCommit(false);
+
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        if($this->entityManager->getConnection()->isTransactionActive()) {
+            $this->entityManager->rollback();
+        }
     }
 }
