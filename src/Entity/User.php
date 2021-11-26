f416c3f1edcfd484c1c4e31ca296c217f0085a76
hello-devs <cirer972@gmail.com>
2021-11-26 17:25:46 +0100

    Map User Class to be Doctrine entity
    Add attribute to User properties
    Create UserRepository class
    Set doctrine to use attribute for mapping in doctrine.yaml
    make migration and migrate
    
    -------------------------------------------
    #for phpstan analyse know doctrine write id:
    require-dev: "phpstan/extension-installer","phpstan/phpstan-doctrine"
    add object-manager.php in tests for phpstan and configure it in phpstan.neon to use it

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,16 +2,37 @@
 
 namespace App\Entity;
 
+use App\Repository\UserRepository;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private ?int $id;
+
+    #[ORM\Column(type: "string", length: 180, unique: true)]
     private string $username;
+
+    #[ORM\Column(type: "string", length: 180, unique: true)]
     private string $email;
+
+    #[ORM\Column(type: "string")]
     private string $password;
+
     /** @var array<string> $roles  */
+    #[ORM\Column(type: "json")]
     private array $roles = [];
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
     public function getUserIdentifier(): string
     {
@@ -55,7 +76,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
 
     /**
-     * @return string
+     * @return string the hashed password
      */
     public function getPassword(): string
     {
