e695601fa5aa122f3b704b33e73f6bf4b6d0009a
hello-devs <cirer972@gmail.com>
2021-11-27 17:23:33 +0100

    usecase: make test request the login url
    we expect route exist, which is not the case at the moment.

--- tests/Api/Security/AuthenticationITest.php
+++ tests/Api/Security/AuthenticationITest.php
@@ -6,44 +6,58 @@ use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AuthenticationITest extends WebTestCase
 {
     private UserPasswordHasherInterface $hasher;
     private EntityManagerInterface $entityManager;
+    private $client;
 
     /** @test
      * @throws Exception
      */
     public function user_should_be_able_to_get_jwt_token_when_authenticated_with_correct_login_and_password(): void
     {
+        $username = 'tester';
+        $email = 'tester@email.com';
         //We have a user in the database
         $user = new User();
         $pwd = $this->hasher->hashPassword($user, 'pwd');
 
         $user
-            ->setUsername('tester')
-            ->setEmail('tester@email.com')
-            ->setPassword($pwd)
-        ;
+            ->setUsername($username)
+            ->setEmail($email)
+            ->setPassword($pwd);
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
         //When the user request the api login endpoint
+        $content = json_encode([
+            'username' => $username,
+            'password' => $pwd
+        ]);
+
 //        $client = static::createClient();
-//        $crawler = $client->request('GET', '/api/login');
-//
+        $crawler = $this->client->request(
+            'POST',
+            '/api/login',
+            [], [], [],
+            $content
+        );
+
+        var_dump($this->client->getResponse()->getStatusCode());
+
         //We expect
+        $this->assertResponseIsSuccessful();
     }
 
     protected function setUp(): void
     {
         parent::setUp();
-
-        self::bootKernel();
+//        self::bootKernel();
+        $this->client = static::createClient();
 
         $container = static::getContainer();
         /** @var UserPasswordHasherInterface $hasher */
