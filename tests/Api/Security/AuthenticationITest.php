8b7a7e74609e7df410da77e72f1e4da8f0ad3bc8
hello-devs <cirer972@gmail.com>
2021-12-03 14:34:29 +0100

    test: Create AbstractApiTestCase to simplify test
    Refactor AuthenticationITest.php test

--- tests/Api/Security/AuthenticationITest.php
+++ tests/Api/Security/AuthenticationITest.php
@@ -2,96 +2,41 @@
 
 namespace Tests\Api\Security;
 
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Tests\Api\AbstractApiTestCase;
 
-class AuthenticationITest extends ApiTestCase
+
+class AuthenticationITest extends AbstractApiTestCase
 {
-    private UserPasswordHasherInterface $hasher;
-    private EntityManagerInterface $entityManager;
-    private Client $client;
-
     /** @test
-     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
      */
     public function user_should_be_able_to_get_jwt_token_when_authenticated_with_correct_login_and_password(): void
     {
+//        //We have a user in the database
         $email = 'tester@email.com';
         $plainPassword = 'pwd';
-        //We have a user in the database
-        $user = new User();
-        $pwd = $this->hasher->hashPassword($user, $plainPassword);
 
-        //todo refactor in a create user function
-        $user
-            ->setEmail($email)
-            ->setPassword($pwd)
-            ->setRoles(['ROLE_USER', 'ROLE_TESTER']);
+        $this->createUserInDatabase($email, $plainPassword);
 
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
 
-        //When the user request the api login endpoint
-        //todo refactor in a request token function
-        $content = json_encode([
+        //When request the api login endpoint with correct credentials
+        $body = [
             'email' => $email,
             'password' => $plainPassword
-        ]);
+        ];
 
-        $jwt = null;
+        $jwt = $this->getToken('get_token', $body);
 
-        $response = $this->client->request(
-            'POST',
-            '/get_token',
-            [
-                'body' => $content,
-                'headers' => [
-                    "CONTENT_TYPE" => "application/json",
-                    "HTTP_ACCEPT" => "application/json"
-                ]
-            ],
-        );
-
-        $responseContent = $response->getContent();
-
-        if ($responseContent
-            && is_array(json_decode($responseContent, true))
-            && array_key_exists('token', json_decode($responseContent, true))) {
-            $jwt = json_decode($responseContent, true)['token'];
-        }
-
-        //We expect the endpoint exist
+        //We expect the endpoint is accessible
         $this->assertResponseIsSuccessful();
-        //We expect response body give us a json web token
+        //We expect response  give us a not null json web token
         $this->assertNotNull($jwt);
-    }
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        self::bootKernel();
-        $this->client = static::createClient();
-
-        $container = static::getContainer();
-        /** @var UserPasswordHasherInterface $hasher */
-        $hasher = $container->get('security.user_password_hasher');
-        $this->hasher = $hasher;
-
-        /** @var EntityManagerInterface $em */
-        $em = $container->get('doctrine.orm.entity_manager');
-        $this->entityManager = $em;
-        $this->entityManager->beginTransaction();
-        $this->entityManager->getConnection()->setAutoCommit(false);
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-        if ($this->entityManager->getConnection()->isTransactionActive()) {
-            $this->entityManager->rollback();
-        }
     }
 }
