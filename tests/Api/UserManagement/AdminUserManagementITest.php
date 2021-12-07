316201c78ae464aadde96dd1168c2436f4858547
hello-devs <cirer972@gmail.com>
2021-12-07 22:30:54 +0100

    usecase: test different role can or cannot access user list
    create: role_hierarchy ROLE_SUPER_ADMIN

--- tests/Api/UserManagement/AdminUserManagementITest.php
+++ tests/Api/UserManagement/AdminUserManagementITest.php
@@ -12,18 +12,17 @@ class AdminUserManagementITest extends AbstractApiTestCase
 {
     /**
      * @test
+     * @dataProvider provideUserWithDifferentRoles
      * @throws TransportExceptionInterface
      * @throws ServerExceptionInterface
      * @throws RedirectionExceptionInterface
      * @throws ClientExceptionInterface
      */
-    public function role_api_admin_is_required_to_manage_user(): void
+    public function role_api_admin_is_required_to_manage_user(string $email, string $password, string $role, int $expectedStatusCode): void
     {
-        $email = "tester@email.com";
-        $plainPwd = "password";
         //We have a user with basic "ROLE_USER"
-        $this->createUserInDatabase($email, $plainPwd);
-        $token = $this->getToken("/get_token", ["email" => $email, "password" => $plainPwd]);
+        $this->createUserInDatabase($email, $password);
+        $token = $this->getToken("/get_token", ["email" => $email, "password" => $password]);
 
         //When we request users list
         $this->requestWithJwt("GET", "/api/users", $token);
@@ -32,5 +31,16 @@ class AdminUserManagementITest extends AbstractApiTestCase
         $this->assertResponseStatusCodeSame(403);
     }
 
-    //todo assert that ROLE_ADMIN can access user list
+    /**
+     * @return array<mixed[]>
+     */
+    public function provideUserWithDifferentRoles(): array
+    {
+        return[
+            ["tester@email.com","password","ROLE_USER",403],
+            ["tester@email.com","password","ROLE_CLIENT",403],
+            ["tester@email.com","password","ROLE_ADMIN",200],
+            ["tester@email.com","password","ROLE_SUPER_ADMIN",200]
+        ];
+    }
 }
