414aa87ce083c151678841cd357f585435a71381
hello-devs <cirer972@gmail.com>
2022-05-21 16:41:21 +0200

    test(BudgetTest): budget initialisation

---
+++ tests/units/BudgetTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\units;
+
+use App\Entity\Budget;
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class BudgetTest extends TestCase
+{
+    /** @test */
+    public function we_can_instantiate_a_budget()
+    {
+        //  Given
+            $user = new User();
+            $budgetName = "New Budget";
+            $startDate = date_create_immutable("2022-05-01");
+
+        //  When
+            $budget = new Budget(name: $budgetName, user: $user, startDate: $startDate);
+
+        //  Then
+        $this->assertInstanceOf(Budget::class, $budget);
+        $this->assertSame($user, $budget->getUser());
+        $this->assertSame($budgetName, $budget->getName());
+        $this->assertSame($startDate, $budget->getStartDate());
+        $this->assertNull($budget->getEndDate());
+    }
+
+
+}
