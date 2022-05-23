258600f27a3a63c88671b6a273b0affb08b6ad2d
hello-devs <cirer972@gmail.com>
2022-05-23 23:14:16 +0200

    test: create class for BudgetManagement integration tests

---
+++ tests/fonctionnals/BudgetManagement/UserBudgetManagementITest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\fonctionnals\BudgetManagement;
+
+use Tests\fonctionnals\AbstractApiTestCase;
+
+class UserBudgetManagementITest extends AbstractApiTestCase
+{
+    public function a_user_can_create_a_budget_then_this_user_can_manage_his_budget(): void
+    {
+        //Given
+
+        //When
+
+        //Then
+    }
+}
