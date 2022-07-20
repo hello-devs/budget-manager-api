d3ad39efbbbb00aebececcc3576fed286bf3fd86
hello-devs <cirer972@gmail.com>
2022-07-20 15:31:18 +0200

    fix: call of BudgetRepository in place of Budget class

--- src/Repository/BudgetRepository.php
+++ src/Repository/BudgetRepository.php
@@ -9,16 +9,16 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @extends ServiceEntityRepository<BudgetRepository>
  *
- * @method BudgetRepository|null find($id, $lockMode = null, $lockVersion = null)
- * @method BudgetRepository|null findOneBy(array $criteria, array $orderBy = null)
- * @method BudgetRepository[]    findAll()
- * @method BudgetRepository[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Budget|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Budget|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Budget[]    findAll()
+ * @method Budget[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class BudgetRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, BudgetRepository::class);
+        parent::__construct($registry, Budget::class);
     }
 
     public function add(BudgetRepository $entity, bool $flush = false): void
