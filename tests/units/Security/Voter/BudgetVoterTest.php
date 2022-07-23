9761342327fcf1914b245720c80e683a147de47a
hello-devs <cirer972@gmail.com>
2022-07-23 09:50:56 +0200

    feat: create BudgetVoter to manage access right to Budget Resource and test it
    feat: create BudgetVoter to manage access right to Budget Resource and test it

---
+++ tests/units/Security/Voter/BudgetVoterTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\units\Security\Voter;
+
+use App\Entity\Budget;
+use App\Entity\User;
+use App\Security\Voter\BudgetVoter;
+use PHPUnit\Framework\TestCase;
+use \Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
+
+class BudgetVoterTest extends TestCase
+{
+
+    /**
+     * @param $user
+     * @param $budget
+     * @param $expectedSecurityReturn
+     * @param $expectedVote
+     * @return void
+     * @dataProvider provideUserBudgetAndVote
+     */
+    public function test_that_we_can_instantiate_budget_voter_class($user, $budget, $expectedSecurityReturn, $expectedVote): void
+    {
+        //  Given
+        $security = $this->createMock(Security::class);
+        $security->method('isGranted')->with('ROLE_ADMIN')->willReturn($expectedSecurityReturn);
+        $token = $this->createMock(TokenInterface::class);
+        $token->method('getUser')->willReturn($user);
+
+        //  When
+        $budgetVoter = new BudgetVoter($security);
+        $vote = $budgetVoter->vote($token, $budget, [BudgetVoter::VIEW]);
+
+        //  Then
+        $this->assertInstanceOf(Voter::class, $budgetVoter);
+        $this->assertEquals($expectedVote, $vote);
+    }
+
+    public function provideUserBudgetAndVote(): \Generator
+    {
+        $user1 = new User();
+        $user2 = new User();
+
+        yield 'user can access' => [
+            $user1,
+            new Budget(name: "unit-test-budget", creator: $user1, startDate: new \DateTimeImmutable("2022-01-01")),
+            false,
+            1
+        ];
+
+        yield 'user cannot access' => [
+            $user1,
+            new Budget(name: "unit-test-budget", creator: $user2, startDate: new \DateTimeImmutable("2022-01-01")),
+            false,
+            -1
+        ];
+
+        yield 'user 1 as admin can' => [
+            $user1,
+            new Budget(name: "unit-test-budget", creator: $user2, startDate: new \DateTimeImmutable("2022-01-01")),
+            true,
+            1
+        ];
+    }
+}
