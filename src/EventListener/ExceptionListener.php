dc136a69df0d862e7bcf49d2adc9fdd8648f1ca6
hello-devs <cirer972@gmail.com>
2022-10-01 15:35:16 +0200

    core: create ExceptionListener service to return JsonResponse depending on the exception class catched

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        //Todo make response specific to exception
+        $response =  new JsonResponse([
+            "title" => "Request Error",
+            "description" => $exception->getMessage(),
+        ]);
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $event->setResponse($response);
+    }
+}
