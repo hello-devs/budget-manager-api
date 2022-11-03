bfd76962202abfebd5b867d8b9c46794da081599
hello-devs <cirer972@gmail.com>
2022-11-03 12:57:40 +0100

    feat: log server error info for debug

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -2,6 +2,7 @@
 
 namespace App\EventListener;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
@@ -9,20 +10,45 @@ use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ExceptionListener
 {
+    public function __construct(private readonly LoggerInterface $logger)
+    {
+    }
+
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
 
+        $title = "Request Error";
+
         //Todo make response specific to exception
-        $response =  new JsonResponse([
-            "title" => "Request Error",
+        $response = new JsonResponse([
+            "title" => $title,
             "description" => $exception->getMessage(),
         ]);
 
         if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
+            $response->setStatusCode($exception->getCode());
+
         } else {
+            $logErrorId = uniqid();
+
             $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            $response->setData([
+                "title" => "SERVER ERROR",
+                "description" => "The server encountered an error while processing your response",
+                "message" => "Please contact administrator with your log error Id to help solve it",
+                "LOG_ERROR_ID" => $logErrorId
+            ]);
+
+            /** @var string $logData */
+            $logData = json_encode([
+                "LOG_ERROR_ID" => $logErrorId,
+                "exception class" => $exception::class,
+                "exception message" => $exception->getMessage(),
+                "exception trace" => $exception->getTraceAsString()
+            ]);
+
+            $this->logger->error($logData);
         }
 
         $event->setResponse($response);
