3e41da3ccbfa1e214b08ddf4e8fa523ea153149d
hello-devs <cirer972@gmail.com>
2022-11-13 10:07:08 +0100

    test: test we can delete BudgetTransaction and Transaction
    BudgetManagementITest:
      add method to test deletion
      refactor reorder method of the class

--- tests/Functional/BudgetManagementITest.php
+++ tests/Functional/BudgetManagementITest.php
@@ -4,6 +4,7 @@ namespace Tests\Functional;
 
 use App\Entity\Budget;
 use App\Entity\BudgetTransaction;
+use App\Entity\Transaction;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
@@ -88,6 +89,7 @@ class BudgetManagementITest extends AbstractApiTestCase
         /**
          * When we request BudgetTransaction deletion
          */
+        $this->checkWeCanDeleteBudgetTransactionAndTransactionIsDeletedTwo($createdBudgetTransaction, $user1Token);
 
 
         /**
@@ -95,6 +97,61 @@ class BudgetManagementITest extends AbstractApiTestCase
          */
         $this->checkThatBudgetCanBeDelete($createdBudget, $user1Token);
     }
+
+    /**
+     * @param string $user1Token
+     * @param array<string, mixed> $budgetData
+     * @return Budget
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function checkValidUserCanCreateBudget(string $user1Token, array $budgetData): Budget
+    {
+        $postResponse = $this->requestWithJwt(
+            "POST",
+            "/api/budgets",
+            $user1Token,
+            $budgetData
+        );
+
+        /** @var Budget $createdBudget */
+        $createdBudget = $this->serializer->deserialize(
+            $postResponse->getContent(),
+            Budget::class,
+            'json',
+            ["groups" => "budget:read"]
+        );
+
+        //then
+        $this->assertResponseStatusCodeSame(201);
+        return $createdBudget;
+    }
+
+    /**
+     * @param Budget $createdBudget
+     * @param string $user1Token
+     * @return void
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function checkWeCanRetrieveCreatedBudgetWithId(Budget $createdBudget, string $user1Token): void
+    {
+        /** @var int $createdBudgetId */
+        $createdBudgetId = $createdBudget->getId();
+
+        $request = $this->getTheCreatedBudget($createdBudgetId, $user1Token);
+        $requestedBudgetId = $request->toArray()["id"];
+
+        //Then
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertEquals($createdBudgetId, $requestedBudgetId);
+    }
+
 
     /**
      * @throws TransportExceptionInterface
@@ -106,6 +163,142 @@ class BudgetManagementITest extends AbstractApiTestCase
             "/api/budgets/$budgetID",
             token: $userToken
         );
+    }
+
+    /**
+     * @param string $user1Token
+     * @param Budget $createdBudget
+     * @param int $budgetTransactionAmount
+     * @param string $budgetTransactionImpactDate
+     * @return BudgetTransaction
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function checkWeCanCreateBudgetTransaction(
+        string $user1Token,
+        Budget $createdBudget,
+        int    $budgetTransactionAmount,
+        string $budgetTransactionImpactDate
+    ): BudgetTransaction {
+        /** @var int $createdBudgetId */
+        $createdBudgetId = $createdBudget->getId();
+
+        $budgetTransactionData = [
+            "budget" => "/api/budgets/" . $createdBudgetId,
+            "transaction" => [
+                "amount" => $budgetTransactionAmount
+            ],
+            "impactDate" => $budgetTransactionImpactDate
+        ];
+
+        $budgetTransactionRequest = $this->requestWithJwt(
+            method: "POST",
+            url: "/api/budget_transactions",
+            token: $user1Token,
+            json: $budgetTransactionData
+        );
+
+        //Then
+        $this->assertResponseStatusCodeSame(201);
+
+        /** @var BudgetTransaction $budgetTransaction * */
+        $budgetTransaction = $this->serializer->deserialize($budgetTransactionRequest->getContent(), BudgetTransaction::class, 'json');
+
+        return $budgetTransaction;
+    }
+
+    /**
+     * @param array<string, mixed> $budgetData
+     * @param Budget $createdBudget
+     * @param string $user1Token
+     * @return void
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function checkThatWeCanUpdateBudgetAndRetrieveUpdatedData(
+        array  $budgetData,
+        Budget $createdBudget,
+        string $user1Token
+    ): void {
+        /** @var int $createdBudgetId */
+        $createdBudgetId = $createdBudget->getId();
+        $budgetData["name"] = "budget-edited-name";
+
+        $updateRequest = $this->requestWithJwt(
+            method: "PUT",
+            url: "/api/budgets/$createdBudgetId",
+            token: $user1Token,
+            json: $budgetData
+        );
+
+        $updateResponse = $updateRequest->toArray();
+        $updatedName = $updateResponse["name"];
+
+        //Then expect only name change
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertSame("budget-edited-name", $updatedName);
+        $this->assertSame($budgetData['startAmount'], $updateResponse['startAmount']);
+    }
+
+    /**
+     * @param BudgetTransaction $createdBudgetTransaction
+     * @param string $user1Token
+     * @param int $budgetTransactionUpdatedAmount
+     * @param string $budgetTransactionUpdatedImpactDate
+     * @return void
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function checkWeCanUpdateBudgetTransactionImpactDateAndAmount(
+        BudgetTransaction $createdBudgetTransaction,
+        string            $user1Token,
+        int               $budgetTransactionUpdatedAmount,
+        string            $budgetTransactionUpdatedImpactDate
+    ): void {
+        $requestBTUpdate = $this->requestWithJwt(
+            "PUT",
+            "/api/budget_transactions/{$createdBudgetTransaction->getId()}",
+            $user1Token,
+            [
+                "transactionAmount" => $budgetTransactionUpdatedAmount,
+                "impactDate" => $budgetTransactionUpdatedImpactDate
+            ]
+        );
+
+        $updatedTransactionData = $requestBTUpdate->toArray();
+
+        $oldBudgetTransactionImpactDate = date_create_immutable($budgetTransactionUpdatedImpactDate);
+        $updatedBudgetTransactionImpactDate = date_create_immutable($updatedTransactionData["impactDate"]);
+
+        $this->assertEquals($oldBudgetTransactionImpactDate, $updatedBudgetTransactionImpactDate);
+        $this->assertEquals($budgetTransactionUpdatedAmount, $updatedTransactionData["transaction"]["amount"]);
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    /**
+     * @param BudgetTransaction $createdBudgetTransaction
+     * @param string $user1Token
+     * @return void
+     * @throws TransportExceptionInterface
+     */
+    public function checkWeCanDeleteBudgetTransactionAndTransactionIsDeletedTwo(BudgetTransaction $createdBudgetTransaction, string $user1Token): void
+    {
+        $this->requestWithJwt("DELETE", "/api/budget_transactions/{$createdBudgetTransaction->getId()}", $user1Token);
+        $this->assertResponseStatusCodeSame(204);
+
+        $this->requestWithJwt("GET", "/api/budget_transactions/{$createdBudgetTransaction->getId()}", $user1Token);
+        $this->assertResponseStatusCodeSame(404);
+
+        $isTransactionDelete = null === $this->entityManager->find(Transaction::class, $createdBudgetTransaction->getTransaction()->getId());
+        $this->assertTrue($isTransactionDelete);
     }
 
     /**
@@ -133,177 +326,5 @@ class BudgetManagementITest extends AbstractApiTestCase
 
         //Then
         $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
-    }
-
-    /**
-     * @param string $user1Token
-     * @param Budget $createdBudget
-     * @param int $budgetTransactionAmount
-     * @param string $budgetTransactionImpactDate
-     * @return BudgetTransaction
-     * @throws ClientExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     */
-    public function checkWeCanCreateBudgetTransaction(
-        string $user1Token,
-        Budget    $createdBudget,
-        int    $budgetTransactionAmount,
-        string $budgetTransactionImpactDate
-    ): BudgetTransaction {
-        /** @var int $createdBudgetId */
-        $createdBudgetId = $createdBudget->getId();
-
-        $budgetTransactionData = [
-            "budget" => "/api/budgets/" . $createdBudgetId,
-            "transaction" => [
-                "amount" => $budgetTransactionAmount
-            ],
-            "impactDate" => $budgetTransactionImpactDate
-        ];
-
-        $budgetTransactionRequest = $this->requestWithJwt(
-            method: "POST",
-            url: "/api/budget_transactions",
-            token: $user1Token,
-            json: $budgetTransactionData
-        );
-
-        //Then
-        $this->assertResponseStatusCodeSame(201);
-
-        /** @var BudgetTransaction $budgetTransaction**/
-        $budgetTransaction = $this->serializer->deserialize($budgetTransactionRequest->getContent(), BudgetTransaction::class, 'json');
-
-        return $budgetTransaction;
-    }
-
-    /**
-     * @param array<string, mixed> $budgetData
-     * @param Budget $createdBudget
-     * @param string $user1Token
-     * @return void
-     * @throws ClientExceptionInterface
-     * @throws DecodingExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     */
-    public function checkThatWeCanUpdateBudgetAndRetrieveUpdatedData(
-        array  $budgetData,
-        Budget  $createdBudget,
-        string $user1Token
-    ): void {
-        /** @var int $createdBudgetId */
-        $createdBudgetId = $createdBudget->getId();
-        $budgetData["name"] = "budget-edited-name";
-
-        $updateRequest = $this->requestWithJwt(
-            method: "PUT",
-            url: "/api/budgets/$createdBudgetId",
-            token: $user1Token,
-            json: $budgetData
-        );
-
-        $updateResponse = $updateRequest->toArray();
-        $updatedName = $updateResponse["name"];
-
-        //Then expect only name change
-        $this->assertResponseStatusCodeSame(200);
-        $this->assertSame("budget-edited-name", $updatedName);
-        $this->assertSame($budgetData['startAmount'], $updateResponse['startAmount']);
-    }
-
-    /**
-     * @param Budget $createdBudget
-     * @param string $user1Token
-     * @return void
-     * @throws ClientExceptionInterface
-     * @throws DecodingExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     */
-    public function checkWeCanRetrieveCreatedBudgetWithId(Budget $createdBudget, string $user1Token): void
-    {
-        /** @var int $createdBudgetId */
-        $createdBudgetId = $createdBudget->getId();
-
-        $request = $this->getTheCreatedBudget($createdBudgetId, $user1Token);
-        $requestedBudgetId = $request->toArray()["id"];
-
-        //Then
-        $this->assertResponseStatusCodeSame(200);
-        $this->assertEquals($createdBudgetId, $requestedBudgetId);
-    }
-
-    /**
-     * @param string $user1Token
-     * @param array<string, mixed> $budgetData
-     * @return Budget
-     * @throws ClientExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     */
-    public function checkValidUserCanCreateBudget(string $user1Token, array $budgetData): Budget
-    {
-        $postResponse = $this->requestWithJwt(
-            "POST",
-            "/api/budgets",
-            $user1Token,
-            $budgetData
-        );
-
-        /** @var Budget $createdBudget */
-        $createdBudget= $this->serializer->deserialize(
-            $postResponse->getContent(),
-            Budget::class,
-            'json',
-            ["groups"=>"budget:read"]
-        );
-
-        //then
-        $this->assertResponseStatusCodeSame(201);
-        return $createdBudget;
-    }
-
-    /**
-     * @param BudgetTransaction $createdBudgetTransaction
-     * @param string $user1Token
-     * @param int $budgetTransactionUpdatedAmount
-     * @param string $budgetTransactionUpdatedImpactDate
-     * @return void
-     * @throws ClientExceptionInterface
-     * @throws DecodingExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     */
-    public function checkWeCanUpdateBudgetTransactionImpactDateAndAmount(
-        BudgetTransaction $createdBudgetTransaction,
-        string $user1Token,
-        int $budgetTransactionUpdatedAmount,
-        string $budgetTransactionUpdatedImpactDate
-    ): void {
-        $requestBTUpdate = $this->requestWithJwt(
-            "PUT",
-            "/api/budget_transactions/{$createdBudgetTransaction->getId()}",
-            $user1Token,
-            [
-                "transactionAmount" => $budgetTransactionUpdatedAmount,
-                "impactDate" => $budgetTransactionUpdatedImpactDate
-            ]
-        );
-
-        $updatedTransactionData = $requestBTUpdate->toArray();
-
-        $oldBudgetTransactionImpactDate = date_create_immutable($budgetTransactionUpdatedImpactDate);
-        $updatedBudgetTransactionImpactDate = date_create_immutable($updatedTransactionData["impactDate"]);
-
-        $this->assertEquals($oldBudgetTransactionImpactDate, $updatedBudgetTransactionImpactDate);
-        $this->assertEquals($budgetTransactionUpdatedAmount, $updatedTransactionData["transaction"]["amount"]);
-        $this->assertResponseStatusCodeSame(200);
     }
 }
